4a5a59797d29e89c99e84adbae1936b1abfaa66b
Samantha Goulding <srgoulding@protonmail.com>
2022-05-27 01:06:25 -0400

    Redux for All

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,4 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
 import { COMMENTS } from "../../app/shared/COMMENTS";
+
+const initialState = {
+  commentsArray: COMMENTS,
+};
+const commentsSlice = createSlice({
+  name: "comments",
+  initialState,
+});
+
+export const commentsReducer = commentsSlice.reducer;
 
 export const selectCommentsByCampsiteId = (campsiteId) => {
   return COMMENTS.filter(
